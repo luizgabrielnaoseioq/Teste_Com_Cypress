a96d108d78c14cc77de4618dbd46bb0f94819745
Nazax <gabrielon6689@gmail.com>
2025-04-16 21:40:42 -0300

    update cod

--- aula2.cy.js
+++ aula2.cy.js
@@ -3,9 +3,25 @@ describe('template spec', () => {
     cy.login(Cypress.env("username"), Cypress.env("password"))
   })
 
-  it('passes', () => {
-    cy.visitx("/products")
-
-    cy.url().should('include', '/products')
+  // Teste único de produto genérico
+  it('createProduct ', () => {
+    cy.createProduct()
   })
+
+  // Repete 100 vezes com nomes únicos
+  Cypress._.times(100, (i) => {
+    it(`Cria o produto #${i + 1}`, () => {
+      cy.createProduct(`Produto ${i + 1}`)
+    })
+  })
+
+  // Outros testes comentados ou futuros
+  // it('passes', () => {
+  //   cy.visitx("/products")
+  //   cy.url().should('include', '/products')
+  // })
+
+  // it('categories', () => {
+  //   cy.categories()
+  // })
 })
